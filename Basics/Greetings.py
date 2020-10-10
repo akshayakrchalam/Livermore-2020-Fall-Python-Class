8255e7ad4dc21c5d82a5be13bc3da8ccba9bbd87
AkshayaKrishna <akshayakrchalam@gmail.com>
2020-10-10 02:04:20 -0700

    Adding User-Friendly Comments

--- Basics/Greetings.py
+++ Basics/Greetings.py
@@ -1,4 +1,8 @@
-name = input("Enter your name: ")
-print("Welcome to Python Programming", name)
+# Greetings by name program
 
+# Accept a name - user input
+name = input("Enter your name: ")
+
+# Print the greeting with name
+print("Welcome to Python Programming", name)
 print(name, "Welcome to Python Programming!")
--- Basics/HelloWorld.py
+++ Basics/HelloWorld.py
@@ -1,1 +1,4 @@
+# Hello World program
+
+# Printing Hello World!
 print("Hello World!")
