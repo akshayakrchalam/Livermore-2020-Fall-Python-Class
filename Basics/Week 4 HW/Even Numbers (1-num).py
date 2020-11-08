856b4e20f9be2ae659ad50689d5ee7a937de6966
AkshayaKrishna <akshayakrchalam@gmail.com>
2020-11-08 08:01:03 -0800

    Week 4 HW Programming Exercises

---
+++ Basics/Week 4 HW/Even Numbers (1-num).py
@@ -0,0 +1,5 @@
+# Print all the even numbers between 1 through num (user input)
+
+num = int(input("Enter a number: "))
+for n in range(2, num+1, 2):
+    print(n)
---
+++ Basics/Week 4 HW/Range 1-10 (Skip 5).py
@@ -0,0 +1,5 @@
+# Using a for loop, have a range from 1 through 10 and skip 5.
+
+for n in range(1, 11):
+    if n != 5:
+        print(n)
